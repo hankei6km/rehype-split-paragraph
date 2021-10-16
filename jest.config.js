7cbc6cf929d85133d67f4ac8d979a883a15d6c2b
hankei6km <gh-hankei6km@outlook.jp>
2021-10-17 03:01:29 +0900

    Setup to ESM package
    It for use unified v10(only ESM).

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
-module.exports = {
+export default {
   roots: ['<rootDir>/src'],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
     '**/?(*.)+(spec|test).+(ts|tsx|js)'
   ],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
+  transform: {},
+  testEnvironment: 'jest-environment-node',
+  // https://kulshekhar.github.io/ts-jest/docs/next/guides/esm-support/
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
   }
 };
